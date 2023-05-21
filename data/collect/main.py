83540cf2f7a40c37f87a20c84e9f3ff5188a8a4c
Nguyễn Thành Văn <ngthvan1612@gmail.com>
2023-05-21 16:13:40 +0700

    van.tien.02.60s

--- data/collect/main.py
+++ data/collect/main.py
@@ -1,18 +1,6 @@
-# from esp32.udp_audio import Esp32AudioUDP
-# from audio import AudioProcessing
-
-# # esp32_audio = Esp32AudioUDP()
-# # esp32_audio.begin()
-# # data = esp32_audio.start_recording(5)
-# # AudioProcessing.write_wav(data, 'raw.temp.mp3')
-# # reduce_noise_data = AudioProcessing.reduce_noise(data)
-# # AudioProcessing.write_wav(reduce_noise_data, 'raw.reduce-noise.temp.mp3')
-# # chunks = AudioProcessing.split_audio(reduce_noise_data)
-# # for i, chunk in enumerate(chunks):
-
-# #   answer = input('Is correct?')
+import sys
 
 from audio.AudioLabeling import AudioLabeling
 
 labeling = AudioLabeling(60)
-labeling.re_config('output_data/van.01.60s')
+labeling.begin('output_data')
