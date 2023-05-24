a77940e38d747ebc1e3f1f427cf26129ab32b831
Nguyễn Thành Văn <ngthvan1612@gmail.com>
2023-05-24 11:49:05 +0700

    dskll

--- data/collect/main.py
+++ data/collect/main.py
@@ -3,5 +3,5 @@ import sys
 from audio.AudioLabeling import AudioLabeling
 
 labeling = AudioLabeling(60)
-labeling.begin('output_data')
-#labeling.re_config('output_data/ngoc.trai.01.60s')
+#labeling.begin('output_data')
+labeling.re_config('output_data/van.tien.05.60s')
