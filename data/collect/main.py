7cadab1ee2cfbbb7e1b7b97cf734a09ffb9add6d
Nguyễn Thành Văn <ngthvan1612@gmail.com>
2023-05-24 12:12:42 +0700

    kdh

--- data/collect/main.py
+++ data/collect/main.py
@@ -3,5 +3,5 @@ import sys
 from audio.AudioLabeling import AudioLabeling
 
 labeling = AudioLabeling(60)
-#labeling.begin('output_data')
-labeling.re_config('output_data/van.phai.02.60s')
+labeling.begin('output_data')
+#labeling.re_config('output_data/van.phai.02.60s')
