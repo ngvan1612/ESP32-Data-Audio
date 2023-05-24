c6d35a0617dff901366440afdf39dba49842c10a
Nguyễn Thành Văn <ngthvan1612@gmail.com>
2023-05-24 11:59:15 +0700

    dnk

--- data/collect/main.py
+++ data/collect/main.py
@@ -4,4 +4,4 @@ from audio.AudioLabeling import AudioLabeling
 
 labeling = AudioLabeling(60)
 #labeling.begin('output_data')
-labeling.re_config('output_data/van.tien.05.60s')
+labeling.re_config('output_data/van.phai.02.60s')
